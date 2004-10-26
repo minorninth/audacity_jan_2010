42ecf12bce933d6c777c72b9488ee02acc01e6ae
vjohnson <>
2004-10-26 19:16:42 +0000

    Upgrade to Soundtouch 1.2.2pre2

--- lib-src/soundtouch/make-win.bat
+++ lib-src/soundtouch/make-win.bat
@@ -7,10 +7,28 @@
 @REM Copyright (c) Olli Parviainen
 @REM File Created: 09/Sep/2003
 @REM
-@REM $Id: make-win.bat,v 1.5 2003/12/25 11:44:43 Olli Exp $ 
+@REM $Id: make-win.bat,v 1.6 2004/01/22 18:08:59 Olli Exp $ 
+
+@if "%MsDevDir%"=="" goto nodevdir
 
 md bin
 md lib
 msdev source\SoundTouch\SoundTouch.dsw /MAKE ALL
 msdev source\example\bpm\bpm.dsw /MAKE ALL
 msdev source\example\SoundStretch\SoundStretch.dsw /MAKE "SoundStretch - Win32 Release"
+
+goto end
+
+:nodevdir
+
+@echo off
+echo *********************************************************************
+echo **
+echo ** ERROR: Visual Studio path not set.
+echo **
+echo ** Run "vcvars32.bat" from Visual Studio installation directory, e.g. 
+echo ** "\Program Files\Microsoft Visual Studio\VC98\Bin", then try again.
+echo **
+echo *********************************************************************
+
+:end
