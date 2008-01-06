1729c8e1ea5e19233d3915a51c5dc5ef02bd5be2
richardash1981 <>
2008-01-06 14:21:19 +0000

    Update to twolame-0.3.10 from upstream (no patches yet)

--- lib-src/twolame/libtwolame/availbits.h
+++ lib-src/twolame/libtwolame/availbits.h
@@ -2,7 +2,7 @@
  *  TwoLAME: an optimized MPEG Audio Layer Two encoder
  *
  *  Copyright (C) 2001-2004 Michael Cheng
- *  Copyright (C) 2004-2005 The TwoLAME Project
+ *  Copyright (C) 2004-2006 The TwoLAME Project
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,9 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ *
+ *  $Id: availbits.h 156 2007-03-20 23:57:35Z nhumfrey $
+ *
  */
 
 #ifndef	_AVAILBITS_H_
@@ -27,3 +29,5 @@ int available_bits ( twolame_options * glopts );
 
 #endif
 
+
+// vim:ts=4:sw=4:nowrap: 
--- lib-src/twolame/libtwolame/crc.h
+++ lib-src/twolame/libtwolame/crc.h
@@ -17,7 +17,9 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *  
+ *
+ *  $Id: crc.h 156 2007-03-20 23:57:35Z nhumfrey $
+ *
  */
 
 #ifndef	_CRC_H_
@@ -27,3 +29,5 @@ void crc_writeheader( unsigned char *bitstream, int bit_count );
 
 #endif
 
+
+// vim:ts=4:sw=4:nowrap: 
